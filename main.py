1d3add4daa7c75f310505b4c2f179e8de4283e4a
aragalucas <126295197+aragalucas@users.noreply.github.com>
2024-10-14 11:15:19 +0000

    criando main

---
+++ main.py
@@ -0,0 +1,9 @@
+""" BANCO DE DADOS
+    -SQL (LINGUAGEM DE CONSULTA ESTRUTURADA)
+    -EXEMPLO:
+        - SELECT * FROM CLIENTES;
+        - iRA CONSULTAR O BD na Tabela Clientes
+
+        -SGBD:
+           -GERENCIAR PERMISSÕES DE ACESSO
+"""
